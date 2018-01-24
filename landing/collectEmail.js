103fe440762f2813e85d30ca0f9c6f33a0ddc9a8
Juan Diego Castrillon <jdcastri@MIT.EDU>
2018-01-23 21:11:25 -0500

    add basic email forwarding

---
+++ landing/collectEmail.js
@@ -0,0 +1,24 @@
+$("form.form-top").submit(function(e) {
+    e.preventDefault();
+    var email = $(".email-top").val();
+    if (email.trim().length > 0) {
+        submit(email);
+    }
+});
+
+$("form.form-bottom").submit(function(e) {
+    e.preventDefault()
+    var email = $(".email-bottom").val()
+    if (email.trim().length > 0) {
+        submit(email);
+    }
+});
+
+function submit(email) {
+    $.get('https://script.google.com/macros/u/0/s/AKfycbzFvk9t35n_CXW-K-4j-MF6ct-gzStadRrh27cNeQu1UGJbopiO/exec',
+        {
+            'email': email
+        }, function(){
+            console.log("success bottom");
+        });
+}
